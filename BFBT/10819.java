6a467d229e297bb84a9470dd8290b71697ae6809
hhp227study <83169005+hhp227study@users.noreply.github.com>
2023-10-07 18:22:53 +0900

    Update 10819.java

--- BFBT/10819.java
+++ BFBT/10819.java
@@ -1,9 +1,42 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        AtomicInteger answer = new AtomicInteger(0);
+        Function<List<Integer>, Integer> calculate = list -> {
+            int result = 0;
+            
+            for (int i = 0; i < array.length - 1; i++) {
+                result += Math.abs(list.get(i) - list.get(i + 1));
+            }
+            return result;
+        };
+        Consumer<List<Integer>> backtracking = new Consumer<>() {
+            @Override
+            public void accept(List<Integer> stack) {
+                if (stack.size() >= array.length) {
+                    answer.set(Math.max(answer.get(), calculate.apply(stack.stream().mapToInt(val -> array[val]).boxed().collect(Collectors.toList()))));
+                    return;
+                }
+                for (int i = 0; i < array.length; i++) {
+                    if (stack.contains(i)) continue;
+                    List<Integer> tempStack = new ArrayList<>(stack);
+
+                    tempStack.add(i);
+                    accept(tempStack);
+                }
+            }
+        };
+
+        Arrays.setAll(array, val -> scanner.nextInt());
+        backtracking.accept(new ArrayList<>());
+        System.out.println(answer.get());
         scanner.close();
     }
 }
